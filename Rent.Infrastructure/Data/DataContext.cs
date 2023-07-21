26798f25986b097165f2c26599117ba161bb181f
Caio Sales <salesrocketdev@outlook.com>
2023-07-21 01:17:23 -0300

    feat: Base login

--- Rent.Infrastructure/Data/DataContext.cs
+++ Rent.Infrastructure/Data/DataContext.cs
@@ -5,18 +5,21 @@ namespace Rent.Infrastructure.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext()
-        {
+        public DataContext() { }
 
-        }
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options) { }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             if (!options.IsConfigured)
             {
-                options.UseSqlServer("Server=localhost,1433;Database=testeDatabase;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+                options.UseSqlServer(
+                    "Server=localhost,1433;Database=Rent;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True"
+                );
             }
         }
+
         public DbSet<Car> Cars => Set<Car>();
         public DbSet<Login> Logins => Set<Login>();
         public DbSet<Customer> Customers => Set<Customer>();
