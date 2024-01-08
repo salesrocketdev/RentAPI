8b92746694c631a003a28264efaeb892bfeb1a4d
Caio Sales <salesrocketdev@outlook.com>
2024-01-07 22:14:02 -0300

    refact: Ajuste authorize

--- Rent.API/Program.cs
+++ Rent.API/Program.cs
@@ -128,7 +128,7 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-// app.UseMiddleware<JwtBlacklistMiddleware>();
+app.UseMiddleware<JwtBlacklistMiddleware>();
 
 app.MapControllers();
 
